68d2c70ba491cc6d8e885ba4ba7707b094430fcb
Eric <ksh117@kakao.com>
2023-11-24 23:17:48 +0900

    filter test

--- src/main/kotlin/filters/hantoeng/HanToEngFilter.kt
+++ src/main/kotlin/filters/hantoeng/HanToEngFilter.kt
@@ -2,13 +2,21 @@ package filters.hantoeng
 
 import org.apache.lucene.analysis.TokenFilter
 import org.apache.lucene.analysis.TokenStream
+import org.apache.lucene.analysis.tokenattributes.CharTermAttribute
+import java.io.IOException
+
+
 
 class HanToEngFilter(input: TokenStream) : TokenFilter(input) {
+
+    private val termAtt: CharTermAttribute = addAttribute(CharTermAttribute::class.java)
 
     override fun incrementToken(): Boolean {
 
         return if (input.incrementToken()) {
             val parserdData: CharSequence = "테스트"
+            termAtt.setEmpty()
+            termAtt.append(parserdData)
             true
         } else {
             false
